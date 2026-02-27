f1e97a0338b6ae92d5497a55292801139195dfe3
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-02-27 18:49:05 +0100

    we can now find the IPA charakters from one IPA word in another IPA word

--- experiments/Ai-Projekt/comparingIPAWords.py
+++ experiments/Ai-Projekt/comparingIPAWords.py
@@ -6,6 +6,16 @@ compIPA = []
 for x in "ʊ̯fɡaːbə":
   compIPA.append(x) # storing my IPA letters seperatly
 
-print("All IPA letter ", compIPA)
-print("last value", compIPA[-1])
+#print("All IPA letter ", compIPA)
+#print("last value", compIPA[-1])
 
+# comparing an IPA Word with IPA letters
+
+IPAword = "ɔpɣaːvə"
+for y in compIPA:
+  if y in IPAword:
+     print("this word is in the IPA word " + y)
+  
+#print (compIPA[2] in IPAword) # Das funktioniert, ich brauche nun logik die den [] bis zum Ende iteriert und Sachen einen Wert adiert
+
+# einen while loop der eine variable eine Zahl bis zum Ende gehen lässt und jedesmal eine Zahl addiert
