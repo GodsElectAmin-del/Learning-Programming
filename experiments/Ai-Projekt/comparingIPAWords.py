fa9647bb62b832034d41ab5f3d9a7907ab347dcc
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-02-27 18:49:55 +0100

    We have a score and are trying to compare the Word to an Array of IPA words

--- experiments/Ai-Projekt/comparingIPAWords.py
+++ experiments/Ai-Projekt/comparingIPAWords.py
@@ -10,11 +10,37 @@ for x in "ʊ̯fɡaːbə":
 #print("last value", compIPA[-1])
 
 # comparing an IPA Word with IPA letters
-
+# TODO THE IPAWord is not one but is an Array of Words and we need step by step compare each
+# TODO We need to add a ranking after each itteration / done
+# Each word need to get there own scpre
+score = 0
+ipa_list = [
+    "ˈɔpɣaːvə",
+    "taːk",
+    "ˈɔpdrɑxt",
+    "klʏs",
+    "kɑrˈʋɛi̯",
+    "ˈtaːkˌstɛlɪŋ",
+    "ˈuːfəŋə(n)",
+    "proːˈbleːm",
+    "ˈpʏzəl",
+    "ˈyːtˌdɑːɣɪŋ",
+    "plɪxt",
+    "vərˈplɪxtɪŋ",
+    "ˈmɪsi",
+    "ʋɛrk",
+    "ɔpˈdrɑxtjə",
+]
+# TODO figure out how i can change the IPA word in an usealble loop
+for x in ipa_list:
+    x = IPAword
+    
 IPAword = "ɔpɣaːvə"
 for y in compIPA:
   if y in IPAword:
      print("this word is in the IPA word " + y)
+     score += 1
+print("the score is ", score)   
   
 #print (compIPA[2] in IPAword) # Das funktioniert, ich brauche nun logik die den [] bis zum Ende iteriert und Sachen einen Wert adiert
 
