f1c60d4173eebf51a438b10632b206fec73da205
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-02 17:08:50 +0100

    We have now an method to convert json to python but i am not sure if the type list is what we want and how to further use the data

--- experiments/Ai-Projekt/exampleGptOutPut.py
+++ experiments/Ai-Projekt/exampleGptOutPut.py
@@ -1,6 +1,13 @@
 import json
-
+'''
 gptoutput = {
    [ {"Word": "Aufgabe","IPA" : "ɔpɣaːvə"}, 
     {"Word": "Aufgabe","IPA" : "taːk"}]
 }
+'''
+
+# example output when using GPT 4.1 nano
+
+gptoutput = '[ {"Word": "taak", "IPA": "taːk"}, {"Word": "opdracht", "IPA": "ˈɔpdrɑxt"}, {"Word": "klus", "IPA": "klʏs"}, {"Word": "opgave", "IPA": "ˈɔpˌɣɑvə"}, {"Word": "taken", "IPA": "ˈtɑkən"}, {"Word": "project", "IPA": "ˈproːʝɛkt"}, {"Word": "missie", "IPA": "mɪsˈsi"}, {"Word": "bezigheid", "IPA": "bəˈzɪɣɦɛit"}, {"Word": "onderneming", "IPA": "ˌɔndərˈneːmɪŋ"}, {"Word": "verantwoordelijkheid", "IPA": "vərˌɑntwɔrdəˌlɛjkɦɛit"}, {"Word": "opdrachtje", "IPA": "ˈɔpdrɑxtjə"}, {"Word": "klusje", "IPA": "ˈklʏsjə"}, {"Word": "puzzel", "IPA": "ˈpʏzəl"}, {"Word": "job", "IPA": "dʒɔb"}, {"Word": "verzoek", "IPA": "vərˈzuːxt"}, {"Word": "projectje", "IPA": "ˈproːʝɛktjə"}, {"Word": "taakje", "IPA": "ˈtaːk.jə"}, {"Word": "uitdaging", "IPA": "œytˈdɑːxɪŋ"}, {"Word": "verantwoordelijkheid", "IPA": "vərˌɑntwɔrdəˌlɛjkɦɛit"}, {"Word": "plan", "IPA": "plɑn"} ]'
+pythonList = json.loads(gptoutput)
+print(pythonList)
