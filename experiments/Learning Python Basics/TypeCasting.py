de51b9f7d461aa6fd4be945ace47c214fcd8b195
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-07 19:14:19 +0100

    learning TypeCasting

---
+++ experiments/Learning Python Basics/TypeCasting.py
@@ -0,0 +1,15 @@
+'''
+Write a function, that for each input integer n returns n as three new types: as string, 
+as integer, and as float.
+
+'''
+
+#TODO We need to make n input integers
+
+def inputInt (x):
+    return x
+
+x = inputInt(4)
+print (f"this i a String {str(x)}, this is a Integer {int(x)} this is a float {float(x)}")
+
+# TODO not how the task is intendet
