139290c6960ebc8d34e34ee521b1fb17f70b8e45
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-07 09:18:11 +0100

    a Konstrukor assigment by GPT

---
+++ exercises/Understanding/Java_Konstruktors2/Main.java
@@ -0,0 +1,8 @@
+package Java_Konstruktors2;
+
+public class Main {
+        public static void main(String[] args) {
+        Person newPerson = new Person("Peter", 20);
+        System.out.println("His Name is " + newPerson.name + "His age is " + newPerson.age);
+    }
+}
---
+++ exercises/Understanding/Java_Konstruktors2/Person.java
@@ -0,0 +1,14 @@
+package Java_Konstruktors2;
+
+public class Person {
+  String name;
+  int age; 
+
+// nun der Konstruktor der von der MAin angefragt wird
+public Person(String name, int age){
+    this.name = name;
+    this.age = age;
+    
+}
+
+}
