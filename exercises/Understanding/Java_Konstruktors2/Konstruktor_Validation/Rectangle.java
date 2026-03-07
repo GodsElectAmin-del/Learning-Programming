0ff6e16594fd836a5985530959a7e9cda794ca7c
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-07 09:35:35 +0100

    Rectangle calculation with Konstruktors

---
+++ exercises/Understanding/Java_Konstruktors2/Konstruktor_Validation/Rectangle.java
@@ -0,0 +1,12 @@
+package Java_Konstruktors2.Konstruktor_Validation;
+
+public class Rectangle {
+    int width;
+    int height;
+    int area;
+
+    public Rectangle(int width, int height){
+            this.area = width * height;
+    }
+
+}
