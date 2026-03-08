a71274294736ad6c3a766f99854201d816274ddf
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-08 10:35:22 +0100

    trying to createbalance calculator by only using stuff named balance i dont fully get it yet

---
+++ exercises/Understanding/Java_Konstruktors2/Account_Balance/Account.java
@@ -0,0 +1,10 @@
+package Account_Balance;
+
+public class Account {
+    double balance;
+
+    public Account(double balance){
+        this.balance = this.balance + balance ;
+    }
+
+}
---
+++ exercises/Understanding/Java_Konstruktors2/Account_Balance/Main.java
@@ -0,0 +1,8 @@
+package Account_Balance;
+
+public class Main {
+    public static void main(String[] args) {
+        Account newAccount = new Account(50);
+        System.out.println("the new Balance is " + newAccount.balance);
+    }
+}
