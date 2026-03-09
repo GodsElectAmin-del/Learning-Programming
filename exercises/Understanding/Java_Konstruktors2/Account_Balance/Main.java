51f83444b2827551b212de359bf0ff7ae6e1ff55
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-09 11:15:24 +0100

    ich habe die Aufgabe eigentlich schon hinreichend gelöst lol

--- exercises/Understanding/Java_Konstruktors2/Account_Balance/Main.java
+++ exercises/Understanding/Java_Konstruktors2/Account_Balance/Main.java
@@ -4,9 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Account newAccount = new Account(0.0);
         System.out.println("the starting Balance is " + newAccount.getbalance());
-        newAccount.setbalance(10.0);
-        System.out.println("the new Balance is " + newAccount.getbalance());
-        newAccount.deposit(30);
+        newAccount.deposit(30.0);
         System.out.println("the real new Balance is " + newAccount.getbalance());
     }
 }
