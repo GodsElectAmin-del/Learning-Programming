28118223cba48bbc68606ef51ef87ae11df63a18
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-11 18:12:33 +0100

    removing the first and last elements of an List

---
+++ experiments/Learning Python Basics/listcopy.py
@@ -0,0 +1,11 @@
+def list_copy(my_list):
+    n = len(my_list)
+    if n <= 2:
+        shorter_list = []
+    else:
+        shorter_list = my_list.copy()
+        shorter_list = shorter_list[1:n-1]
+    return my_list, shorter_list, 
+print(list_copy([1,2,3,4,5]))
+print(list_copy([1]))
+print(list_copy([]))
