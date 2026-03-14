8f98baea65bd0292ba34006c4ff44eba6aee377d
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-14 22:23:08 +0100

    the basic logic works now but the score is beoken, it seems like we are compaing html stuff and not actually the IPA letters

--- experiments/Ai-Projekt/comparingIPAWordsAgain.py
+++ experiments/Ai-Projekt/comparingIPAWordsAgain.py
@@ -20,7 +20,7 @@ def Ipa_score(ipa_list):
         Word = item.get("Word", "")
         IPA = item.get("IPA", "")
 
-        Score = sum( 1 for y in compIPA if y in IPA)
+        Score = sum( (1* len(compIPA) / len(Word)) for y in compIPA if y in IPA)
         ipa_Scorelist[2][index] = Score
         ipa_Scorelist[0][index] = Word
         ipa_Scorelist[1][index] = IPA
@@ -43,6 +43,8 @@ print(Ipa_score(ipa_list))
 '''
 
 '''
-
+1 * lamda
+lamda = len(compIPA) / len/(Word)
 '''
 #TODO I want a multi dimensional list where in the first is the normal word, 2end is the IPA word and 3rd is the score
+#TODO the score is broken, because the code compares html stuff and not actually IPA elements
