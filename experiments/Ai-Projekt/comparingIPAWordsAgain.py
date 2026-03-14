30d356063359509c882f10cf3f7492230e1634b5
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-14 22:09:06 +0100

    i now get the best Word too, i want to get a better calculation so that there wont be multiple best scores

--- experiments/Ai-Projekt/comparingIPAWordsAgain.py
+++ experiments/Ai-Projekt/comparingIPAWordsAgain.py
@@ -10,6 +10,7 @@ ipa_list = [{'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}, {'Word': 'zonnege
 ipa_test = {'Word': 'zonnebloem', 'IPA': 'ˈzɔ.nə.bloːm'}
 n = len(ipa_list)
 ipa_Scorelist = [[0] * n for _ in range(3)]
+highest_score = 0
 
 def Ipa_score(ipa_list):
     #i have the lenght of the IPA list
@@ -24,7 +25,16 @@ def Ipa_score(ipa_list):
         ipa_Scorelist[0][index] = Word
         ipa_Scorelist[1][index] = IPA
 
-    return n, Word, IPA, ipa_Scorelist
+    # TODO now i need the highest score
+    finalScore = ipa_Scorelist[2]
+    highest_score = 0
+    for i, ThisFinalScore in enumerate(finalScore): # Dont now why that works
+        ThisFinalScore = finalScore[i] 
+        if ThisFinalScore > highest_score:
+            highest_score = ThisFinalScore
+            thisIndex = i
+    TheBestWord = ipa_Scorelist[0][thisIndex]
+    return n, Word, IPA, ipa_Scorelist, highest_score, thisIndex, TheBestWord
 
 print(Ipa_score(ipa_list))
 '''
@@ -32,4 +42,7 @@ print(Ipa_score(ipa_list))
     ipa = z.get("IPA", "")
 '''
 
+'''
+
+'''
 #TODO I want a multi dimensional list where in the first is the normal word, 2end is the IPA word and 3rd is the score
