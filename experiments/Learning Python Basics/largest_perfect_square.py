bf36bba208ef0b6614c3c11c12bbc86d5b03fc0d
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-16 19:41:07 +0100

    trying to find the largest square root of any give numbers

--- experiments/Learning Python Basics/largest_perfect_square.py
+++ experiments/Learning Python Basics/largest_perfect_square.py
@@ -1,5 +1,8 @@
 import math
 def largest_perfect_square(numbers):
-        myNumber = math.sqrt(numbers)
-        return myNumber
-print(largest_perfect_square(25))
+    squaredNumber = None
+    for i in numbers:
+        if math.sqrt(i) > squaredNumber or squaredNumber == None:
+            squaredNumber = math.sqrt(i)
+    return squaredNumber
+print(largest_perfect_square([20,34,45,25,60,81]))
