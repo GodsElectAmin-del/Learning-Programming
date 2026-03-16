612e6a9b264df3b3bbfac6713c1f90a1aa008ed0
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-16 19:43:54 +0100

    Learning: the Order of wrighting an if statement matter in python

--- experiments/Learning Python Basics/largest_perfect_square.py
+++ experiments/Learning Python Basics/largest_perfect_square.py
@@ -2,7 +2,7 @@ import math
 def largest_perfect_square(numbers):
     squaredNumber = None
     for i in numbers:
-        if math.sqrt(i) > squaredNumber or squaredNumber == None:
+        if squaredNumber == None or math.sqrt(i) > squaredNumber:
             squaredNumber = math.sqrt(i)
     return squaredNumber
 print(largest_perfect_square([20,34,45,25,60,81]))
