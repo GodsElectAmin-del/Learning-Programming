36b27bb2600d917ef48647b9eb40494efbacc679
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-17 19:54:24 +0100

    i am stuck because matplot lib wont work i might continue on the working platform before continuuing

---
+++ experiments/Learning Python Basics/plot_square.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+def plot_square():
+    baum = plt.plot()
+    return baum
+plot_square.plt.show([1,2,3],[5,6,7])
+
+# TODO get a working matplitlib fucking waste of TIMe 
