8a74bc2729102be000536096884aea6fb7363dcb
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-17 19:37:30 +0100

    i make a break here

--- experiments/Learning Python Basics/largest_perfect_square.py
+++ experiments/Learning Python Basics/largest_perfect_square.py
@@ -1,11 +1,11 @@
 import math
 def largest_perfect_square(numbers):
-    squaredNumber = None
+    squaredNumber = 1
     for i in numbers:
-        if squaredNumber == None or math.sqrt(i) == int(math.sqrt(i)) and i > squaredNumber:
+        if  math.sqrt(i) == int(math.sqrt(i)) and i > squaredNumber:
             squaredNumber = i
     return squaredNumber
-print(largest_perfect_square([3,5,7,13]))
+print(largest_perfect_square([3,5,7,13,25]))
 
 # TODO how can i check if i have a perfect square lol
 # TODO create a check if something is can be calculated by a * a // a = int(math.sqrt())
