2656d85226d49265c9c3ed37eaf10023d7040e41
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-18 18:24:23 +0100

    graph was inverted lol

--- experiments/Learning Python Basics/largest_perfect_square.py
+++ experiments/Learning Python Basics/largest_perfect_square.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 def plot_square(): 
     y_line = [0,0,0,0,0,0,0,0,0,0]
     for i in range(10):
-        x = i * i + 3
+        x = (i * i) + 3
         y_line[i] = x
-    plt.plot(y_line, [1,2,3,4,5,6,7,8,9,10])
+    plt.plot([1,2,3,4,5,6,7,8,9,10], y_line)
 
 plot_square()
 plt.show()
