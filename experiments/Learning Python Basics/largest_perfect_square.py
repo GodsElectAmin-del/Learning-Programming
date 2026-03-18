362371210cc39e7bcfe1320ede7c4d5c7acde26b
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-18 18:16:36 +0100

    i amtrying to calculate the a list with the wright numbers

--- experiments/Learning Python Basics/largest_perfect_square.py
+++ experiments/Learning Python Basics/largest_perfect_square.py
@@ -1,11 +1,15 @@
-import math
-def largest_perfect_square(numbers):
-    squaredNumber = 1
-    for i in numbers:
-        if  math.sqrt(i) == int(math.sqrt(i)) and i > squaredNumber:
-            squaredNumber = i
-    return squaredNumber
-print(largest_perfect_square([3,5,7,13,25]))
+def plot_square(y_line): 
+    #y_line = [0,0,0,0,0,0,0,0,0,0]
+    for i in range(10):
+        x = i * i + 3
+        for ii in range(10):
+            y_line[ii] = x
+    #y_line = [0,0,0,0,0,0,0,0,0,0]
+    #plt.plot(y_line, [1,2,3,4,5,6,7,8,9,10])
+    return y_line
+
+plot_square([0,0,0,0,0,0,0,0,0,0])
+#plt.show()
 
 # TODO how can i check if i have a perfect square lol
 # TODO create a check if something is can be calculated by a * a // a = int(math.sqrt())
