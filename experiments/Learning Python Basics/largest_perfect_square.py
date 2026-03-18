7fb01d7bc1ae57f529be8f8a241beeffe0aae334
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-18 18:22:14 +0100

    my working code i think

--- experiments/Learning Python Basics/largest_perfect_square.py
+++ experiments/Learning Python Basics/largest_perfect_square.py
@@ -1,15 +1,14 @@
-def plot_square(y_line): 
-    #y_line = [0,0,0,0,0,0,0,0,0,0]
+import matplotlib.pyplot as plt
+
+def plot_square(): 
+    y_line = [0,0,0,0,0,0,0,0,0,0]
     for i in range(10):
         x = i * i + 3
-        for ii in range(10):
-            y_line[ii] = x
-    #y_line = [0,0,0,0,0,0,0,0,0,0]
-    #plt.plot(y_line, [1,2,3,4,5,6,7,8,9,10])
-    return y_line
+        y_line[i] = x
+    plt.plot(y_line, [1,2,3,4,5,6,7,8,9,10])
 
-plot_square([0,0,0,0,0,0,0,0,0,0])
-#plt.show()
+plot_square()
+plt.show()
 
 # TODO how can i check if i have a perfect square lol
 # TODO create a check if something is can be calculated by a * a // a = int(math.sqrt())
