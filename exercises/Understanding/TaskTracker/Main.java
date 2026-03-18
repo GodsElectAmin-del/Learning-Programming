7a45efe8626ddb5711d71334421473e57adf82ca
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-18 10:42:16 +0100

    first itteration with user input

--- exercises/Understanding/TaskTracker/Main.java
+++ exercises/Understanding/TaskTracker/Main.java
@@ -15,19 +15,35 @@ public class Main {
         newTask2.markDone(false);
         System.out.println(newTask2);
         */
+        TaskService service = new TaskService();
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter your Command");
         String command = myObj.nextLine();
         String trimeedCommand = command.trim();
         String myCommand = trimeedCommand.indexOf(" ");
-        If (myCommand != -1);{
+        if (myCommand != -1);{
             String theCOmmand = input.substring(0, myCommand);
             String actualCommand = input.substring(myCommand + 1);
+        }
+        if (theCommand == "add") {
+            service.addTask(actualCommand);
+        }
+        if (theCommand == "list"){
+            service.listTask();
+        }
+        if (theCommand == "done"){
+            try{
+            int numActualCommand = Integer.parseInt(actualCommand);
+            service.markDoneTask(numActualCommand);}
+            catch (NumberFormatException e){
+                System.out.println("you are slow!");
+            }
         }
         // using indexOf and substring
 
 
        // Service.addTask(myObj);
+       /* 
         TaskService service = new TaskService();
         service.addTask("Amazon Sci-Fi kucken ");
         service.addTask("The Wolf of Wall Street");
@@ -37,7 +53,7 @@ public class Main {
         service.markDoneTask(1);
         service.markDoneTask(2);
         service.listTask();
-
+        */
     }
     
 }
