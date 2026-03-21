3ff6beae38ca195e7a541dcdaf2ab5bc580efe27
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-21 15:17:46 +0100

    made this fakulät calc because Claude asked me how i would make one

---
+++ exercises/Claude_Learning/fakultaet.java
@@ -0,0 +1,12 @@
+public class fakultaet {
+    public static void main(String[] args) {
+        int result = 1;
+        for (int i = 5; i > 0;){
+            result = result * i;
+            i--;
+        }
+
+        System.out.println("The result is " + result);
+    }
+    
+}
