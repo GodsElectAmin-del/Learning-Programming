a8c63c9f64f1c0fc30efcfc96666d4baef2da646
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-21 19:22:26 +0100

    so the code works for array zeros and ones

---
+++ experiments/Learning Python Basics/my_arrays.py
@@ -0,0 +1,7 @@
+import numpy as np 
+def my_arrays(n):
+    array_zeros = np.zeros(n)
+    array_ones = np.ones(n)
+    #array_twoes = np.fives(n)
+    return array_zeros,array_ones
+print(my_arrays(4))
