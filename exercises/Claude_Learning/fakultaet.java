63c465ddda4c1b5574be47d1785c063329b8e2e0
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-21 15:25:54 +0100

    not how claude wanted it lol

--- exercises/Claude_Learning/fakultaet.java
+++ exercises/Claude_Learning/fakultaet.java
@@ -1,12 +1,16 @@
 public class fakultaet {
     public static void main(String[] args) {
-        int result = 1;
+        int fakultaet_5 = 1;
         for (int i = 5; i > 0;){
-            result = result * i;
+            fakultaet_5 = fakultaet_5 * i;
             i--;
         }
 
-        System.out.println("The result is " + result);
+        System.out.println("The result is " + fakultaet_5);
+        int fakultaet_6 = 6 * fakultaet_5;
+        System.out.println("The result is " + fakultaet_6);
     }
+
+    // solving fakulätt 6 mit fakultät 4
     
 }
