9d028a761e4903de54940c90bf100fcf9d4289e5
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-21 19:39:33 +0100

    it workds now for 5 too

--- experiments/Learning Python Basics/my_arrays.py
+++ experiments/Learning Python Basics/my_arrays.py
@@ -2,6 +2,9 @@ import numpy as np
 def my_arrays(n):
     array_zeros = np.zeros(n)
     array_ones = np.ones(n)
+    array_fives = np.zeros(n)
+    for i in range(len(array_fives)):
+        array_fives[i] = 5
     #array_twoes = np.fives(n)
-    return array_zeros,array_ones
+    return array_zeros,array_ones,array_fives
 print(my_arrays(4))
