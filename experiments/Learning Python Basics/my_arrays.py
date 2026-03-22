9fc0e7256193cb8d9ca2b341934c7e2ae56b6c63
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-22 15:55:09 +0100

    adding a if funktion to abort when n smaller 0

--- experiments/Learning Python Basics/my_arrays.py
+++ experiments/Learning Python Basics/my_arrays.py
@@ -1,10 +1,11 @@
 import numpy as np 
 def my_arrays(n):
+    if n <= 0:
+        return None
     array_zeros = np.zeros(n)
     array_ones = np.ones(n)
     array_fives = np.zeros(n)
     for i in range(len(array_fives)):
         array_fives[i] = 5
-    #array_twoes = np.fives(n)
     return array_zeros,array_ones,array_fives
 print(my_arrays(4))
