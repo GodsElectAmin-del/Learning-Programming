0c624ecee3dbcc494a35433e1a12c43926697ef1
Elect Okosun <gokosun@techfak.uni-bielefeld.de>
2026-03-24 14:51:48 +0100

    simple loop, we actually needed a search but not gpt :C

---
+++ exercises/Understanding/AUD/PÜ1_simple_loop.java
@@ -0,0 +1,11 @@
+class PÜ1_simple_loop {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 10;
+        while (x < y) {
+            x++;
+        }
+        System.out.println("this is now our x " + x);
+    }
+
+}
